ff0967e90b6ec6bed92f8ce16047e4de77857766
Biktimirko <ixsi.biktimirov@yandex.ru>
2022-09-26 16:48:58 +0500

    Update DateCreator.cs

--- DateCreator.cs
+++ DateCreator.cs
@@ -11,8 +11,14 @@ public class DateCreator : MonoBehaviour{
 	
 	
 	public GameObject ContentMinute;
+	public GameObject ScrollMinute;
+	
 	public GameObject ContentMonth;
+	public GameObject ScrollMonth;
+	
 	public GameObject ContentDate;
+	public GameObject ScrollDate;
+	
 	public GameObject TaskTime;
 	
 	public GameObject ButPref;
@@ -34,17 +40,16 @@ public class DateCreator : MonoBehaviour{
 	
 	float 		SizeH =24f; 		//количество объектов в списке
 	float 		inSizeH=5f;			//количество одновременно показываемых объектов
-	bool		freeSpace=true;		//евли истина, то сверху и снизу будут пыстые места
+	bool		freeSpaceH=true;		//евли истина, то сверху и снизу будут пыстые места
 	float 		SpaceInt=0;
 	
-		if (freeSpace){
+		if (freeSpaceH){
 			SpaceInt = inSizeH-1;
 			SizeH = SizeH + SpaceInt;
 		}
-	
-	HourList = new List<GameObject>();
+		
 	ContentHour.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		, -(SizeH/inSizeH)+1);
-	ScrollHour.GetComponent<Scrollbar>().numberOfSteps=(int)SizeH;
+	ScrollHour.GetComponent<Scrollbar>().numberOfSteps=24;
 		for (int a = 0; a < (int)(SizeH-SpaceInt); a++){
 			GameObject clone = Instantiate(ButPref) as GameObject;
 			clone.GetComponent<RectTransform>().SetParent(ContentHour.GetComponent<RectTransform>());
@@ -53,8 +58,8 @@ public class DateCreator : MonoBehaviour{
 			clone.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,((SpaceInt/2)+a)*(1f/SizeH));
 			clone.GetComponent<RectTransform>().anchorMax = new Vector2 	(1		,((SpaceInt/2)+a)*(1f/SizeH)+(1f/SizeH));
 			actionTarget =clone.GetComponent<ButSkript>();
-			actionTarget.setInfo(((int)SizeH-1-a)+"");
-			clone.GetComponentInChildren<TMP_Text>().text=((int)SizeH-1-a)+"";
+			actionTarget.setInfo(((int)SizeH-SpaceInt-1-a)+"");
+			clone.GetComponentInChildren<TMP_Text>().text=((int)SizeH-SpaceInt-1-a)+"";
 			clone.GetComponent<Button>().onClick.AddListener(() => ClickHour(clone));
 		}
 		
@@ -65,22 +70,90 @@ public class DateCreator : MonoBehaviour{
 	//
 		
 	//Minute set
-	MinuteList = new List<GameObject>();
-	ContentMinute.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,-11);
-		for (int a = 0; a < 60; a++){
+	
+	float 		SizeM =60f; 		//количество объектов в списке
+	float 		inSizeM=5f;			//количество одновременно показываемых объектов
+	bool		freeSpaceM=true;		//евли истина, то сверху и снизу будут пыстые места
+	float 		SpaceIntM=0;
+	
+		if (freeSpaceM){
+			SpaceIntM = inSizeM-1;
+			SizeM = SizeM + SpaceIntM;
+		}
+		
+	ContentMinute.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		, -(SizeM/inSizeM)+1);
+	ScrollMinute.GetComponent<Scrollbar>().numberOfSteps=60;
+		for (int a = 0; a < (int)(SizeM-SpaceIntM); a++){
 			GameObject clone = Instantiate(ButPref) as GameObject;
 			clone.GetComponent<RectTransform>().SetParent(ContentMinute.GetComponent<RectTransform>());
 			clone.GetComponent<RectTransform>().offsetMin = new Vector2 	(0		,0);
 			clone.GetComponent<RectTransform>().offsetMax = new Vector2 	(0		,0);
-			clone.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,a*(1f/60f));
-			clone.GetComponent<RectTransform>().anchorMax = new Vector2 	(1		,a*(1f/60f)+(1f/60f));
+			clone.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,((SpaceIntM/2)+a)*(1f/SizeM));
+			clone.GetComponent<RectTransform>().anchorMax = new Vector2 	(1		,((SpaceIntM/2)+a)*(1f/SizeM)+(1f/SizeM));
 			actionTarget =clone.GetComponent<ButSkript>();
-			actionTarget.setInfo((59-a)+"");
-			clone.GetComponentInChildren<TMP_Text>().text=(59-a)+"";
-			clone.GetComponent<Button>().onClick.AddListener(() => ClickMinute(clone));
+			actionTarget.setInfo(((int)SizeM-SpaceIntM-1-a)+"");
+			clone.GetComponentInChildren<TMP_Text>().text=((int)SizeM-SpaceIntM-1-a)+"";
+			clone.GetComponent<Button>().onClick.AddListener(() => ClickHour(clone));
+		}
+	
+	
+	//Month set
+	/*
+	float 		SizeM =24f; 		//количество объектов в списке
+	float 		inSizeM=5f;			//количество одновременно показываемых объектов
+	bool		freeSpaceM=true;		//евли истина, то сверху и снизу будут пыстые места
+	float 		SpaceIntM=0;
+	*/
+	SizeM =24f; 
+		if (freeSpaceM){
+			SpaceIntM = inSizeM-1;
+			SizeM = SizeM + SpaceIntM;
+		}
+		
+	ContentMinute.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		, -(SizeM/inSizeM)+1);
+	ScrollMinute.GetComponent<Scrollbar>().numberOfSteps=60;
+		for (int a = 0; a < (int)(SizeM-SpaceIntM); a++){
+			GameObject clone = Instantiate(ButPref) as GameObject;
+			
+			clone.GetComponent<RectTransform>().SetParent(ContentMinute.GetComponent<RectTransform>());
+			clone.GetComponent<RectTransform>().offsetMin = new Vector2 	(0		,0);
+			clone.GetComponent<RectTransform>().offsetMax = new Vector2 	(0		,0);
+			clone.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,((SpaceIntM/2)+a)*(1f/SizeM));
+			clone.GetComponent<RectTransform>().anchorMax = new Vector2 	(1		,((SpaceIntM/2)+a)*(1f/SizeM)+(1f/SizeM));
+			actionTarget =clone.GetComponent<ButSkript>();
+			actionTarget.setInfo("");
+			clone.GetComponentInChildren<TMP_Text>().text=(System.DateTime.Now.ToString("yyyy-MM"));//(int)SizeM-SpaceIntM-1-a)+"";
+			clone.GetComponent<Button>().onClick.AddListener(() => ClickHour(clone));
+		}
+	
+	
+	//Date set
+	/*
+	float 		SizeM =60f; 		//количество объектов в списке
+	float 		inSizeM=5f;			//количество одновременно показываемых объектов
+	bool		freeSpaceM=true;		//евли истина, то сверху и снизу будут пыстые места
+	float 		SpaceIntM=0;
+	*/
+		if (freeSpaceM){
+			SpaceIntM = inSizeM-1;
+			SizeM = SizeM + SpaceIntM;
+		}
+		
+	ContentMinute.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		, -(SizeM/inSizeM)+1);
+	ScrollMinute.GetComponent<Scrollbar>().numberOfSteps=60;
+		for (int a = 0; a < (int)(SizeM-SpaceIntM); a++){
+			GameObject clone = Instantiate(ButPref) as GameObject;
+			clone.GetComponent<RectTransform>().SetParent(ContentMinute.GetComponent<RectTransform>());
+			clone.GetComponent<RectTransform>().offsetMin = new Vector2 	(0		,0);
+			clone.GetComponent<RectTransform>().offsetMax = new Vector2 	(0		,0);
+			clone.GetComponent<RectTransform>().anchorMin = new Vector2 	(0		,((SpaceIntM/2)+a)*(1f/SizeM));
+			clone.GetComponent<RectTransform>().anchorMax = new Vector2 	(1		,((SpaceIntM/2)+a)*(1f/SizeM)+(1f/SizeM));
+			actionTarget =clone.GetComponent<ButSkript>();
+			actionTarget.setInfo(((int)SizeM-SpaceIntM-1-a)+"");
+			clone.GetComponentInChildren<TMP_Text>().text=((int)SizeM-SpaceIntM-1-a)+"";
+			clone.GetComponent<Button>().onClick.AddListener(() => ClickHour(clone));
 		}
 	}
-
 
 	private void ClickMinute(GameObject obj){
 		actionTarget =obj.GetComponent<ButSkript>();
